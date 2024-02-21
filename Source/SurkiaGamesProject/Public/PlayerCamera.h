af9ce5f51d104a84544f33c09ebe640e4b2f054a
Giacomo Trenna <ae2342@student.jamk.fi>
2024-02-21 10:09:49 +0200

    Removed default camera spawn and added custom camera spawn and BP

---
+++ Source/SurkiaGamesProject/Public/PlayerCamera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "PlayerCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SURKIAGAMESPROJECT_API APlayerCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
--- Source/SurkiaGamesProject/SurkiaGamesProjectCharacter.cpp
+++ Source/SurkiaGamesProject/SurkiaGamesProjectCharacter.cpp
@@ -10,6 +10,7 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Materials/Material.h"
 #include "Engine/World.h"
+#include "PlayerCamera.h"
 
 ASurkiaGamesProjectCharacter::ASurkiaGamesProjectCharacter()
 {
@@ -27,25 +28,18 @@ ASurkiaGamesProjectCharacter::ASurkiaGamesProjectCharacter()
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 
-	// Create a camera boom...
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
-	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
-	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
-
-	// Create a camera...
-	TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
-	TopDownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
-	TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
-
 	// Activate ticking in order to update the cursor every frame.
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.bStartWithTickEnabled = true;
 }
 
+void ASurkiaGamesProjectCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
 void ASurkiaGamesProjectCharacter::Tick(float DeltaSeconds)
 {
+	
     Super::Tick(DeltaSeconds);
 }
